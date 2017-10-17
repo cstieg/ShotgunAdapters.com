5d8fa958e55181dfc403fa90275cb174b73192f5
cstieg <cstieg4899@yahoo.com>
2017-10-16 23:08:10 -0400

    Add project files.

---
+++ ShotgunAdapters.com/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShotgunAdapters.com.Startup))]
+namespace ShotgunAdapters.com
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
